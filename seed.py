3f8f93064efb0e7dcb4ab66f503317e6fb6263bd
Guillermo Guirao Aguilar <contact@guillermoguiraoaguilar.com>
2017-05-20 21:40:02 +0200

    Added sample data

--- seed.py
+++ seed.py
@@ -1,7 +1,12 @@
 import os
 import json
+import datetime
 
 from server.database.connector import db
+
+
+def date(value):
+    return datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.000Z")
 
 
 for datafile in os.listdir('data'):
@@ -9,6 +14,14 @@ for datafile in os.listdir('data'):
     if extension == 'json':
         with open('data/' + datafile) as data:
             data = json.load(data)
+        if name == 'cards':
+            for card in data:
+                for field in ['LastMove', 'DateArchived', 'LastActivity']:
+                    if card[field]:
+                        card[field] = date(card[field])
+        elif name == 'events':
+            for event in data:
+                event['DateTime'] = date(event['DateTime'])
         db[name].drop()
         if data:
             print("Populating {} {}".format(len(data), name.replace('_', ' ')))
