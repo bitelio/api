f906adc7333636a5f23295b72dae415d025c1722
Guillermo Guirao Aguilar <contact@guillermoguiraoaguilar.com>
2017-05-21 11:05:52 +0200

    Added board tests

---
+++ server/database/test/test_board.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+from .. import board
+
+
+class Board(TestCase):
+    def test_get_board(self):
+        data = board.get(100000000)
+        self.assertIn('CardTypes', data)
+        self.assertIn('ClassesOfService', data)
+
+    def test_user_not_found(self):
+        data = board.get(300000000)
+        self.assertIsNone(data)
