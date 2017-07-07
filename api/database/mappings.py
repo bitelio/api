f8c847cfcfd08372fd15a8585c03359024ecdb3b
Guillermo Guirao Aguilar <contact@guillermoguiraoaguilar.com>
2017-07-07 17:36:19 +0200

    Minor changes

--- api/database/mappings.py
+++ api/database/mappings.py
@@ -2,5 +2,5 @@
 # -*- coding: utf-8 -*-
 
 
-roles = {1: 'reader', 2: 'user', 3: '-', 4: 'administrator'}
+roles = {1: 'reader', 2: 'user', 3: 'manager', 4: 'administrator'}
 priorities = {1: 'low', 2: 'normal', 3: 'high', 4: 'critical'}
