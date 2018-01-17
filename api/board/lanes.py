59c86814b53c7521a9d54fe8806bfac9e2ab7f24
Guillermo Guirao Aguilar <contact@guillermoguiraoaguilar.com>
2018-01-17 16:38:48 +0100

    Changed response structure for lanes endpoint

--- api/board/lanes.py
+++ api/board/lanes.py
@@ -5,7 +5,7 @@ from api.board import BoardHandler, BoardModel
 
 
 class LanesModel(BoardModel):
-    Stage = StringType(choices=["backlog", "wip", "archive"])
+    Stage = StringType(choices=["backlog", "wip", "archive"], default="wip")
 
     fields = ["Id", "Title", "Index", "Width", "Orientation",
               "ChildLaneIds", "ParentLaneId"]
@@ -19,4 +19,12 @@ class LanesModel(BoardModel):
 class LanesHandler(BoardHandler):
     async def load(self):
         cursor = self.db.lanes.find(self.model.query, self.model.projection)
-        return await cursor.to_list(None)
+        return self.arrange(await cursor.to_list(None))
+
+    @staticmethod
+    def arrange(lanes):
+        table = {lane["Id"]: lane for lane in lanes}
+        for lane in lanes:
+            lane["ChildLanes"] = [table[id] for id in lane["ChildLaneIds"]]
+            del lane["ChildLaneIds"]
+        return [lane for lane in lanes if not lane["ParentLaneId"]]
--- test/board/test_lanes.py
+++ test/board/test_lanes.py
@@ -2,7 +2,7 @@ from json import loads
 from unittest import TestCase
 
 from test import APITestCase
-from api.board.lanes import LanesModel
+from api.board.lanes import LanesModel, LanesHandler
 
 
 class TestLanesModel(TestCase):
@@ -19,13 +19,28 @@ class TestLanesHandler(APITestCase):
     def test_get_lanes(self):
         response = self.post({"BoardId": 100000000})
         assert response.code == 200
-        assert len(loads(response.body)) == 12
+        assert len(loads(response.body)) == 4
 
     def test_board_not_found(self):
         response = self.post({"BoardId": 300000000})
         assert response.code == 404
 
-    # def test_wip_lanes(self):
-        # response = self.post({"BoardId": 100000000, "Stage": "wip"})
-        # assert response.code == 200
-        # assert len(loads(response.body)) == 10
+    def test_arrange_lanes(self):
+        lanes = [
+          {"Id": 1, "Index": 0, "ParentLaneId": None, "ChildLaneIds": [2, 3]},
+          {"Id": 2, "Index": 0, "ParentLaneId": 1, "ChildLaneIds": []},
+          {"Id": 3, "Index": 0, "ParentLaneId": 1, "ChildLaneIds": [4]},
+          {"Id": 4, "Index": 0, "ParentLaneId": 3, "ChildLaneIds": []},
+          {"Id": 5, "Index": 0, "ParentLaneId": None, "ChildLaneIds": []}
+        ]
+        expected = [
+          {"Id": 1, "Index": 0, "ParentLaneId": None, "ChildLanes": [
+              {"Id": 2, "Index": 0, "ParentLaneId": 1, "ChildLanes": []},
+              {"Id": 3, "Index": 0, "ParentLaneId": 1, "ChildLanes": [
+                {"Id": 4, "Index": 0, "ParentLaneId": 3, "ChildLanes": []},
+              ]},
+          ]},
+          {"Id": 5, "Index": 0, "ParentLaneId": None, "ChildLanes": []}
+        ]
+        result = LanesHandler.arrange(lanes)
+        assert result == expected
