2bb78cc9af4dbc746aa99c58e456d33a41f605d8
Guillermo Guirao Aguilar <contact@guillermoguiraoaguilar.com>
2018-01-29 19:22:30 +0100

    Adapted lanes response to new layout parameters

--- api/board/lanes.py
+++ api/board/lanes.py
@@ -5,10 +5,7 @@ from api.board import BoardHandler, BoardModel
 
 
 class LanesModel(BoardModel):
-    Stage = StringType(choices=["backlog", "wip", "archive"], default="wip")
-
-    fields = ["Id", "Title", "Index", "Width", "Orientation",
-              "ChildLaneIds", "ParentLaneId"]
+    fields = ["Id", "Title", "Top", "Width", "Height", "Left", "ChildLaneIds"]
 
     @property
     def query(self):
@@ -19,12 +16,4 @@ class LanesModel(BoardModel):
 class LanesHandler(BoardHandler):
     async def load(self):
         cursor = self.db.lanes.find(self.model.query, self.model.projection)
-        return self.arrange(await cursor.to_list(None))
-
-    @staticmethod
-    def arrange(lanes):
-        table = {lane["Id"]: lane for lane in lanes}
-        for lane in lanes:
-            lane["ChildLanes"] = [table[id] for id in lane["ChildLaneIds"]]
-            del lane["ChildLaneIds"]
-        return [lane for lane in lanes if not lane["ParentLaneId"]]
+        return await cursor.to_list(None)
--- test/board/test_lanes.py
+++ test/board/test_lanes.py
@@ -8,7 +8,7 @@ from api.board.lanes import LanesModel, LanesHandler
 class TestLanesModel(TestCase):
     @staticmethod
     def test_query():
-        query = {"BoardId": 100000000, "Stage": "wip"}
+        query = {"BoardId": 100000000}
         lanes = LanesModel(query)
         assert lanes.query == query
 
@@ -19,28 +19,8 @@ class TestLanesHandler(APITestCase):
     def test_get_lanes(self):
         response = self.post({"BoardId": 100000000})
         assert response.code == 200
-        assert len(loads(response.body)) == 4
+        assert len(loads(response.body)) == 12
 
     def test_board_not_found(self):
         response = self.post({"BoardId": 300000000})
         assert response.code == 404
-
-    def test_arrange_lanes(self):
-        lanes = [
-          {"Id": 1, "Index": 0, "ParentLaneId": None, "ChildLaneIds": [2, 3]},
-          {"Id": 2, "Index": 0, "ParentLaneId": 1, "ChildLaneIds": []},
-          {"Id": 3, "Index": 0, "ParentLaneId": 1, "ChildLaneIds": [4]},
-          {"Id": 4, "Index": 0, "ParentLaneId": 3, "ChildLaneIds": []},
-          {"Id": 5, "Index": 0, "ParentLaneId": None, "ChildLaneIds": []}
-        ]
-        expected = [
-          {"Id": 1, "Index": 0, "ParentLaneId": None, "ChildLanes": [
-              {"Id": 2, "Index": 0, "ParentLaneId": 1, "ChildLanes": []},
-              {"Id": 3, "Index": 0, "ParentLaneId": 1, "ChildLanes": [
-                {"Id": 4, "Index": 0, "ParentLaneId": 3, "ChildLanes": []},
-              ]},
-          ]},
-          {"Id": 5, "Index": 0, "ParentLaneId": None, "ChildLanes": []}
-        ]
-        result = LanesHandler.arrange(lanes)
-        assert result == expected
