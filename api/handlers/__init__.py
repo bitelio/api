de2bacabf788fb1f044768df4770d48b1729fcf4
Guillermo Guirao Aguilar <contact@guillermoguiraoaguilar.com>
2020-02-09 10:10:01 +0100

    Removed walrus operator to fix linting

--- api/handlers/__init__.py
+++ api/handlers/__init__.py
@@ -27,7 +27,8 @@ class MetricsHandler(BaseHandler):
         registry = self.registry
         accept = self.request.headers.get("Accept")
         encoder, content_type = choose_encoder(accept)
-        if name := self.get_argument("name", None):  # noqa
+        name = self.get_argument("name", None)
+        if name:
             registry = registry.restricted_registry(name)
         output = encoder(registry)
         self.set_header("Content-Type", content_type)
